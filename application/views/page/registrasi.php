e25d6e95e1ea314f04181f4a2525a85cd8e03742
Shell Rean <reanyusufali@gmail.com>
2019-07-06 18:41:00 +0700

    Register Field fix

--- application/views/page/registrasi.php
+++ application/views/page/registrasi.php
@@ -6,26 +6,77 @@
 			<div class="card-body">
 				<form>
 				  <div class="form-group row">
-				    <label class="col-sm-2 col-form-label">Nama</label>
-				    <div class="col-sm-10">
-				      <input type="text" class="form-control" placeholder="Nama">
+				    <label class="col-sm-3 col-form-label">Username</label>
+				    <div class="col-sm-9">
+				      <input type="text" class="form-control" placeholder="Username" name="username" required>
 				    </div>
 				  </div>
 				  <div class="form-group row">
-				    <label class="col-sm-2 col-form-label">Alamat</label>
-				    <div class="col-sm-10">
-				      <textarea class="form-control" placeholder="Alamat"></textarea>
+				    <label class="col-sm-3 col-form-label">Password</label>
+				    <div class="col-sm-9">
+				      <input type="password" class="form-control" placeholder="Password" name="password" required>
 				    </div>
 				  </div>
 				  <div class="form-group row">
-					<label class="col-sm-2 col-form-label">Tanggal lahir</label>
-				    <div class="col-sm-10">
-				      <input type="date" class="form-control">
+				    <label class="col-sm-3 col-form-label">Nama lengkap</label>
+				    <div class="col-sm-9">
+				      <input type="text" class="form-control" placeholder="Nama lengkap" name="password" required>
 				    </div>
 				  </div>
 				  <div class="form-group row">
-				  	<label class="col-sm-2 col-form-label">Jenis Kelamin</label>
-				  	<div class="col-sm-10">
+					<label class="col-sm-3 col-form-label">Tanggal lahir</label>
+				    <div class="col-sm-9">
+				      <input type="date" class="form-control" name="tgl_lahir" required>
+				    </div>
+				  </div>
+				  <div class="form-group row">
+				  	<label class="col-sm-3 col-form-label">Agama</label>
+				  	<div class="col-sm-9">
+				  		<select class="form-control" name="agama">
+				  			<option value="islam">Islam</option>
+				  			<option value="kristen">Kristen</option>
+				  			<option value="hindu">Hindu</option>
+				  		</select>
+				  	</div>
+				  </div>
+				  <div class="form-group row">
+					<label class="col-sm-3 col-form-label">Kewarganegaraan</label>
+				    <div class="col-sm-9">
+				      <input type="text" class="form-control" placeholder="Kewarganegaraan" name="kewarganegaraan" required>
+				    </div>
+				  </div>
+				  <div class="form-group row">
+				  	<label class="col-sm-3 col-form-label">Jenjang pendidikan</label>
+				  	<div class="col-sm-9">
+				  		<select class="form-control" name="jenjang_pendidikan">
+				  			<option value="sd">SD</option>
+				  			<option value="smp">SMP</option>
+				  			<option value="smak">SMA/SMK</option>
+				  			<option value="sarjana">Sarjana</option>
+				  		</select>
+				  	</div>
+				  </div>
+				  <div class="form-group row">
+					<label class="col-sm-3 col-form-label">Pas foto ijazah</label>
+				    <div class="col-sm-9">
+				      <input type="file" class="form-control" placeholder="foto_ijazah" name="foto_ijazah">
+				    </div>
+				  </div>
+				  <div class="form-group row">
+					<label class="col-sm-3 col-form-label">Umur</label>
+				    <div class="col-sm-9">
+				      <input type="text" class="form-control" placeholder="Umur" name="umur" required>
+				    </div>
+				  </div>
+				  <div class="form-group row">
+					<label class="col-sm-3 col-form-label">Status</label>
+				    <div class="col-sm-9">
+				      <input type="text" class="form-control" placeholder="Status" name="status" required>
+				    </div>
+				  </div>
+				  <div class="form-group row">
+				  	<label class="col-sm-3 col-form-label">Jenis kelamin</label>
+				  	<div class="col-sm-9">
 				  		<select class="form-control">
 				  			<option>Laki laki</option>
 				  			<option>Perempuan</option>
@@ -33,35 +84,24 @@
 				  	</div>
 				  </div>
 				  <div class="form-group row">
-					<label class="col-sm-2 col-form-label">No KTP</label>
-				    <div class="col-sm-10">
+				    <label class="col-sm-3 col-form-label">Alamat lengkap</label>
+				    <div class="col-sm-9">
+				      <textarea class="form-control" placeholder="Alamat" name="alamat"></textarea>
+				    </div>
+				  </div>
+				  <div class="form-group row">
+					<label class="col-sm-3 col-form-label">No HP</label>
+				    <div class="col-sm-9">
 				      <input type="text" class="form-control" placeholder="Nama">
 				    </div>
 				  </div>
 				  <div class="form-group row">
-					<label class="col-sm-2 col-form-label">Email</label>
-				    <div class="col-sm-10">
-				      <input type="text" class="form-control" placeholder="Nama">
+					<label class="col-sm-3 col-form-label">Pas foto</label>
+				    <div class="col-sm-9">
+				      <input type="file" class="form-control" name="foto_pelamar">
 				    </div>
 				  </div>
-				  <div class="form-group row">
-					<label class="col-sm-2 col-form-label">Nama</label>
-				    <div class="col-sm-10">
-				      <input type="text" class="form-control" placeholder="Nama">
-				    </div>
-				  </div>
-				  <div class="form-group row">
-					<label class="col-sm-2 col-form-label">Nama</label>
-				    <div class="col-sm-10">
-				      <input type="text" class="form-control" placeholder="Nama">
-				    </div>
-				  </div>
-				  <div class="form-group row">
-					<label class="col-sm-2 col-form-label">Nama</label>
-				    <div class="col-sm-10">
-				      <input type="text" class="form-control" placeholder="Nama">
-				    </div>
-				  </div>
+				  
 				</form>
 			</div>
 			<div class="card-footer">
